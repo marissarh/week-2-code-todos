526c83de9325e98329edfd51d71e9951f7d1e661
James Norton <mrjamesnorton@gmail.com>
2024-01-19 13:18:47 -0800

    todos

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import TodoListItems from './TodoListItems';
+
+function App() {
+  const [itemToAdd, setItemToAdd] = useState("");
+  const [todos, setTodos] = useState(['wash dishes', 'mop floor', 'teach class', 'cook food', 'tune guitar']);
+
+  const addItem = (event) => {
+    event.preventDefault();
+    
+    if (itemToAdd === '') {
+      window.alert('You must type in a value')
+      return;
+    }
+
+    setTodos([...todos, itemToAdd]);
+    setItemToAdd('');
+  }
+
+  return (
+    <>
+     <h1>Todos</h1>
+      <form onSubmit={addItem}>
+        <label htmlFor="todo-input">Todo: </label>
+        <input id="todo-input" type="text" onChange={event => setItemToAdd(event.target.value)} value={itemToAdd}/>
+        <button type="submit">Add Todo</button>
+      </form>
+     <TodoListItems todos={todos}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/TodoListItems.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const TodoListItems = ({todos}) => {
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={`todo-${index}`}>{todo}</li>
+          )
+        })}
+      </ul>
+
+    </div>
+
+ 
+  )
+}
+
+TodoListItems.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default TodoListItems;
